b457ff9eabdcfd88a1f46972f00be776579a1123
Sebangel <angeljavicueche@gmail.com>
2023-07-14 19:43:04 -0400

    Se agrega exponential Search

--- Sort_And_Search.h
+++ Sort_And_Search.h
@@ -2,8 +2,9 @@
 #include <iostream>
 #include "struct.h"
 
-int partition_FirstName(inventario arr[], int low, int high) {
-  inventario pivot = arr[high];
+//Particiones para el Quick Sort
+int partition_FirstName(datos arr[], int low, int high) {
+  datos pivot = arr[high];
   int i = low - 1;
   int j = 0;
   for (j = low; j <= high - 1; j++) {
@@ -21,8 +22,8 @@ int partition_FirstName(inventario arr[], int low, int high) {
   return (i + 1);
 }
 
-int partition_ID(inventario arr[], int low, int high) {
-  inventario pivot = arr[high];
+int partition_ID(datos arr[], int low, int high) {
+  datos pivot = arr[high];
   int i = low - 1;
   int j = 0;
   for (j = low; j <= high - 1; j++) {
@@ -35,7 +36,8 @@ int partition_ID(inventario arr[], int low, int high) {
   return (i + 1);
 }
 
-void quicksort(inventario arr[], int low, int high, int caso){
+// Quick Sort
+void quicksort(datos arr[], int low, int high, int caso){
   if (low < high) {
     int pi;
     switch(caso){
@@ -52,4 +54,36 @@ void quicksort(inventario arr[], int low, int high, int caso){
     quicksort(arr, low, pi - 1, caso);
     quicksort(arr, pi + 1, high, caso);
   }
+}
+
+//Exponential Search
+int binarySearch(datos arr[], int l, int r, string x, string y)
+{
+    if (r >= l)
+    {
+        int mid = l + (r - l)/2;
+        if (arr[mid].nombre == x){
+          if (arr[mid].apellido == y){
+              return mid;
+          }
+          else if (arr[mid].apellido > y)
+           return binarySearch(arr, l, mid-1, x, y);
+        }
+        if (arr[mid].nombre > x)
+            return binarySearch(arr, l, mid-1, x, y);
+        return binarySearch(arr, mid+1, r, x, y);
+    }
+    return -1;
+}
+
+int exponentialSearch(datos arr[], int n, string x, string y)
+{
+    if (arr[0].nombre == x)
+        return 0;
+ 
+    int i = 1;
+    while (i < n && arr[i].nombre <= x)
+        i = i*2;
+ 
+    return binarySearch(arr, i/2,min(i, n-1), x, y);
 }
