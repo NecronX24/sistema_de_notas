4d9a5ee667303aa99824640ca31e53f1113cce8c
Sebangel <angeljavicueche@gmail.com>
2023-07-14 20:42:52 -0400

    Se modifica para que funcione el menu principal y lea la funcion de mostrar estudiante

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "struct.h"
 #include "Sort_And_Search.h"
+#include "Mostrar_Estudiante.h"
 using namespace std;
 
 void volver_a_inicio();
@@ -14,8 +15,7 @@ int main(){
         return 0;
     }
     csv_to_struct();
-    infile.close();
-    quicksort(datos_est, 0, estudiantes-1, 1);
+    inicio();
     outfile.open("Prueba.csv");
     struct_to_csv();
     return 0;
@@ -25,8 +25,7 @@ void menu_eleccion(int temp){
     switch (temp)
     {
     case 1:
-    quicksort(datos_est, 0, estudiantes-1, 1);
-        //Importa si cambia el orden inicial?. Preguntar como plantea la busqueda por estudiante
+        menu_mostrar_Estudiante();
         break;
 
     case 2:
@@ -82,15 +81,15 @@ void volver_a_inicio(){
 
 void inicio(){
     int eleccion;
-    cout<< "Programa de Sistema de Notas de Estudiantes, elige tu opcion"<<endl;
+    cout<< "Programa de Sistema de Notas de Estudiantes, elige tu opcion:"<<endl;
     cout<< "1. Mostrar un estudiante o todos y su nota promedio"<<endl;
     cout<< "2. Mostrar materias y la cantidad de alumnos (total, raspados y aprobados)" <<endl;
     //por genero para mas puntos
     cout<< "3. Editar informacion de estudiantes" <<endl;
     cout<< "4. Agregar estudiante nuevo" <<endl;
-    cout<< "5. Top 10 estudiantes por materia" <<endl;
-    cout<< "6. Crear boleta" <<endl;
-    cout<< "7. Reporte de ventas (100 mejores)" <<endl;
+    cout<< "5. Promedio de notas por materia" <<endl;
+    cout<< "6. Top 10 estudiantes por materia" <<endl;
+    cout<< "7. Crear boleta" <<endl;
     cout<< ">> "; 
     cin>> eleccion;
     menu_eleccion(eleccion);
