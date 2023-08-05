e61fdcfa6f7c6e7631ff131090fb1c0d2a0ab641
Sebangel <angeljavicueche@gmail.com>
2023-08-05 17:14:26 -0400

    refactor: se cambia el menu de inicio para no usar recursividad

--- main.cpp
+++ main.cpp
@@ -9,9 +9,7 @@
 #include "Boleta.h"
 using namespace std;
 
-void volver_a_inicio();
 void inicio();
-void menu_eleccion(int temp);
 
 int main(){
     infile.open("Datos_Estudiantes_revisado.csv");
@@ -26,79 +24,83 @@ int main(){
     return 0;
 }
 
-void menu_eleccion(int temp){
-    switch (temp)
-    {
-    case 1:
-        menu_mostrar_Estudiante();
-        break;
-
-    case 2:
-        Materias_y_Alumnos();
-        break;
-
-    case 3:
-        editar();
-        break;
-
-    case 4:
-        if (estudiantes>=lenght){
-            extender_datos_est();
-        }
-        agregar(estudiantes);
-        estudiantes++;
-        break;
-
-    case 5:
-        promedio_notas();
-        break;
-
-    case 6:
-        top();
-        break;
-
-    case 7:
-        hacer_boleta();
-        break;
-
-    default:
-        cout<<"Eleccion erronea"<<endl;
-        inicio();
-        break;
-    }
-    volver_a_inicio();
-}
-
-void volver_a_inicio(){
-    cout<< "Quiere volver al menu para seleccionar otra opcion? (1 para si y 0 para no)"<<endl;
-    cout<<">> ";
-    int eleccion;
-    cin>> eleccion;
-    switch(eleccion){
-        case 0:
-            struct_to_csv();
-            break;
-        case 1:
-            inicio();
-            break;
-        default:
-            cout<<"Elige bien";
-            volver_a_inicio();
-            break;
-    }
-}
+   
 
 void inicio(){
-    int eleccion;
-    cout<< "Programa de Sistema de Notas de Estudiantes, elige tu opcion:"<<endl;
-    cout<< "1. Mostrar un estudiante o todos y su nota promedio"<<endl;
-    cout<< "2. Mostrar materias y la cantidad de alumnos (total, raspados y aprobados)" <<endl;
-    cout<< "3. Editar informacion de estudiantes" <<endl;
-    cout<< "4. Agregar estudiante nuevo" <<endl;
-    cout<< "5. Promedio de notas por materia" <<endl;
-    cout<< "6. Top 10 estudiantes por materia" <<endl;
-    cout<< "7. Crear boleta" <<endl;
-    cout<< ">> "; 
-    cin>> eleccion;
-    menu_eleccion(eleccion);
+     int ciclo;
+    do{
+        ciclo=0;
+        int ciclo_dos;
+        do{
+            ciclo_dos=0;
+            int eleccion;
+            cout<< "Programa de Sistema de Notas de Estudiantes, elige tu opcion:"<<endl;
+            cout<< "1. Mostrar un estudiante o todos y su nota promedio"<<endl;
+            cout<< "2. Mostrar materias y la cantidad de alumnos (total, raspados y aprobados)" <<endl;
+            cout<< "3. Editar informacion de estudiantes" <<endl;
+            cout<< "4. Agregar estudiante nuevo" <<endl;
+            cout<< "5. Promedio de notas por materia" <<endl;
+            cout<< "6. Top 10 estudiantes por materia" <<endl;
+            cout<< "7. Crear boleta" <<endl;
+            cout<< ">> "; 
+            cin>> eleccion;
+            switch (eleccion){
+            case 1:
+                menu_mostrar_Estudiante();
+                break;
+
+            case 2:
+                Materias_y_Alumnos();
+                break;
+
+            case 3:
+                editar();
+                break;
+
+            case 4:
+                if (estudiantes>=lenght){
+                    extender_datos_est();
+                }
+                agregar(estudiantes);
+                estudiantes++;
+                break;
+
+            case 5:
+                promedio_notas();
+                break;
+
+            case 6:
+                top();
+                break;
+
+            case 7:
+                hacer_boleta();
+                break;
+
+            default:
+                cout<<"Eleccion erronea"<<endl;
+                ciclo_dos=1;
+                break;
+            }
+        }while(ciclo_dos==1);
+        do{
+            cout<< "Quiere volver al menu para seleccionar otra opcion? (1 para si y 0 para no)"<<endl;
+            cout<<">> ";
+            int eleccion;
+            cin>> eleccion;
+            ciclo_dos=0;
+            switch(eleccion){
+                case 0:
+                    struct_to_csv();
+                    break;
+                case 1:
+                    ciclo=1;
+                    break;
+                default:
+                    cout<<"Elige bien"<<endl;
+                    ciclo_dos =1;
+                    break;
+            }
+        }while (ciclo_dos==1);
+    }while(ciclo == 1);
 }
